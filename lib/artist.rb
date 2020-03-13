acbcf1d1bf1d4f435261ea7a16d502a89c556cc3
Charles Pelton <charliejp0311@gmail.com>
2020-03-13 00:45:27 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,15 +14,15 @@ class Artist
   def save
     @@all << self
   end
-  
+
   def self.all
     @@all
   end
-  
+
   def self.destroy_all
     @@all.clear
   end
-  
+
   def self.create(name)
     self.new(name)
   end
