da56d7ecf5db4dbca7fbe42c9fe8aa20185fad08
Charles Pelton <charliejp0311@gmail.com>
2020-03-13 00:42:04 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,21 +11,21 @@ class Artist
     save
   end
 
-  def save
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
-
-  def self.destroy_all
-    @@all.clear
-  end
-
-  def self.create(name)
-    self.new(name)
-  end
+  # def save
+  #   @@all << self
+  # end
+  # 
+  # def self.all
+  #   @@all
+  # end
+  # 
+  # def self.destroy_all
+  #   @@all.clear
+  # end
+  # 
+  # def self.create(name)
+  #   self.new(name)
+  # end
 
   def add_song(song)
     if song.artist == nil
